010a6934c51cf02c37c4efb0c15a1744c8b9b013
Saitama <sahilmanchanda1996@gmail.com>
2023-05-19 21:42:12 +0530

    add color variables & improve script

--- compare.sh
+++ compare.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Define some color variables
+GREEN='\033[1m\033[32m'
+ORANGE='\033[1m\033[38;5;214m'
+PURPLE='\033[1m\033[38;5;140m'
+PEACH='\e[1;38;2;255;204;153m'
+BLUE='\e[1;1;34m'
+NC='\033[0m' # No Color
+
+# Show APK Compare Tool text in middle
+clear
+echo ""
+COLUMNS=$(tput cols)
+title="APK Compare Tool"
+printf "${PURPLE}%*s\n${NC}" $(((${#title} + $COLUMNS) / 2)) "$title"
+echo ""
+
 # Set the names and paths of the apk files using positional parameters
 first_apk="$1"
 second_apk="$2"
@@ -12,12 +28,13 @@ if [ -f "$first_apk" ] && [ -f "$second_apk" ]; then
     # Check if apktool and diff tool are available
     if command -v apktool >/dev/null 2>&1 && command -v diff >/dev/null 2>&1; then
         # Decode the apk files using apktool
-        echo "Decompiling both APK files..."
+        echo -e "${GREEN}Decompiling both APK files...${NC}"
+        echo ""
         apktool d -f -o first_apk "$first_apk" >/dev/null 2>&1
         apktool d -f -o second_apk "$second_apk" >/dev/null 2>&1
 
         # Ask the user which changes they want to compare
-        echo -e "\033[38;5;208mWhich changes do you want to compare?\033[0m"
+        echo -e "${ORANGE}Which changes do you want to compare?${NC}"
         echo "1. Resources"
         echo "2. Smali"
         echo "3. Everything"
@@ -27,26 +44,38 @@ if [ -f "$first_apk" ] && [ -f "$second_apk" ]; then
         if [ $choice -eq 1 ]; then
             output=$(diff --color=always -r first_apk/res second_apk/res)
             if [ -z "$output" ]; then
-                echo -e "\033[38;5;208mNo changes were found in resources.\033[0m"
+                echo ""
+                echo -e "${BLUE}No changes were found in resources.${NC}"
+                echo ""
             else
                 echo "$output" | tee resources_changes_$log_file
-                echo -e "\033[38;5;208mLogs of the comparison result have been saved!\033[0m"
+                echo ""
+                echo -e "${GREEN}Logs of the comparison result have been saved!${NC}"
+                echo ""
             fi
         elif [ $choice -eq 2 ]; then
             output=$(diff --color=always -r first_apk/smali second_apk/smali)
             if [ -z "$output" ]; then
-                echo -e "\033[38;5;208mNo changes were found in smali.\033[0m"
+                echo ""
+                echo -e "${BLUE}No changes were found in smali.${NC}"
+                echo ""
             else
                 echo "$output" | tee smali_changes_$log_file
-                echo -e "\033[38;5;208mLogs of the comparison result have been saved!\033[0m"
+                echo ""
+                echo -e "${GREEN}Logs of the comparison result have been saved!${NC}"
+                echo ""
             fi
         elif [ $choice -eq 3 ]; then
             output=$(diff --color=always -r first_apk second_apk)
             if [ -z "$output" ]; then
-                echo -e "\033[38;5;208mNo changes were found.\033[0m"
+                echo ""
+                echo -e "${BLUE}No changes were found.${NC}"
+                echo ""
             else
                 echo "$output" | tee every_changes_$log_file
-                echo -e "\033[38;5;208mLogs of the comparison result have been saved!\033[0m"
+                echo ""
+                echo -e "${GREEN}Logs of the comparison result have been saved!${NC}"
+                echo ""
             fi
         else
             echo "Invalid input."
@@ -57,9 +86,9 @@ if [ -f "$first_apk" ] && [ -f "$second_apk" ]; then
 
     else
         # Echo a message if one or both tools are not available
-        echo -e "\033[31;1mApktool or diff tool not found. Please make sure you have both tools in your system.\033[0m"
+        echo -e "${PEACH}Apktool or diff tool not found. Please make sure you have both tools in your system.${NC}"
     fi
 else
     # Echo a message if one or both files do not exist
-    echo "One or both apk files do not exist."
+    echo -e "${PEACH}One or both apk files do not exist.${NC}"
 fi
